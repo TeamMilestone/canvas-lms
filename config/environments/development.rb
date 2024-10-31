8975e98a9d83db612275cecab5d82122a40f353d
malkoG <rijgndqw012@gmail.com>
2024-10-31 18:57:44 +0900

    orbstack 지원

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -83,6 +83,7 @@ Rails.application.configure do
 
   # allow docker dev setup to use http proxy
   config.hosts << ENV["VIRTUAL_HOST"] if ENV["VIRTUAL_HOST"]
+  config.hosts << "web.canvas-lms.orb.local"
 
   # allow any additional hosts
   ENV["ADDITIONAL_ALLOWED_HOSTS"]&.split(",")&.each do |host|
